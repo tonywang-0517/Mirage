19462d8b5fa55461ed1b0c906694597c6dc70148
Tony <anthonywang0517@gmail.com>
2025-08-25 12:04:53 +1200

    final

--- mirage/global_config.py
+++ mirage/global_config.py
@@ -4,14 +4,14 @@
 from easydict import EasyDict
 
 Config = EasyDict({
-    "control_delay": False, #ok
-    "randomize_body_mass": False, #ok
-    "randomize_joint_parameters": False, #ok
+    "control_delay": True, #ok
+    "randomize_body_mass": True, #ok
+    "randomize_joint_parameters": True, #ok
     "randomize_body_com": False, #bug
-    "randomize_actuator_gains": False, #ok
+    "randomize_actuator_gains": True, #ok
     "randomize_rfi_lim": False,#bug
     "random_push_robot": False, #not necessary
-    "randomize_physics_scene_gravity": False, #ok
+    "randomize_physics_scene_gravity": True, #ok
     "enabled_self_collisions": True
 })
 
